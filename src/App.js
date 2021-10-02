8e111787723ee7a0e072728486d05e76a54bc7ed
Shulabh Dixit <shulabhdixit143@gmail.com>
2021-10-02 22:28:08 +0530

    card created

--- src/App.js
+++ src/App.js
@@ -2,12 +2,9 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import SearchRecipe from './components/SearchRecipe';
-require('dotenv').config()
 const App=()=>{
-  console.log(process.env.APPLICATION_ID)
-  console.log(process.env.APPLICATION_KEY)
   return (
-    <div className="container-fluid bg-success" style={{width:"100vw",height:"100vh"}}>
+    <div className="container-fluid bg-success" style={{width:"100vw",height:"auto",minHeight:"100vh"}}>
       <h1 className="text-center text-white text-uppercase p-3">Recipe Search Application</h1>
       <Router>
         <Switch>
