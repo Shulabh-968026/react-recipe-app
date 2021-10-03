7df11759953722ff9b803d7249c4ad421bb64aeb
Shulabh Dixit <shulabhdixit143@gmail.com>
2021-10-03 21:53:45 +0530

    project complete

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -1,17 +1,16 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 const RecipeCard=(props)=>{
     console.log(props.recipe)
 
     return (
         <div className="col-sm-4 mt-3">
-            <div className="card" style={{width:"400px"}}>
-                <img className="card-img-top" src={props.recipe.recipe.image} alt="Card image"/>
+            <div className="card" style={{maxWidth:"450px",minWeight:"200px"}}  key={props.recipe.recipe.label} >
+                <img className="card-img-top img-thumbnail" src={props.recipe.recipe.image} alt="Card image"/>
                     <div className="card-body">
                         <h4 className="card-title">{props.recipe.recipe.label}</h4>
+                        <p><strong>Calories</strong> {props.recipe.recipe.calories} KCal.</p>
                         <span className="card-text float-start">Desh Type --  {props.recipe.recipe.dishType}</span>
                         <span className="card-text float-end">Meal Type -- {props.recipe.recipe.mealType}</span>
-                         <a href="#" className="btn btn-primary mt-3">See Full Details</a>
                     </div>
             </div>
         </div>
